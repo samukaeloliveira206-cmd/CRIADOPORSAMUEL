e1f6bac0b08e212476b38551f8f42ab2182860ac
Samukael <samukaeloliveira206@gmail.com>
2026-06-30 19:32:07 -0300

    Fix task addition alert condition

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let tarefa = JSON.parse(localStorage.getItem("@listaTarefas")) || [];
 botaoAdicionar.onclick = adicionarTarefa;
 
 function adicionarTarefa() {
-  if (!!!campoTarefa.value.trim()) {
+  if (!!campoTarefa.value.trim()) {
     alert("Você não digitou uma tarefa");
     return false;
   } else {
